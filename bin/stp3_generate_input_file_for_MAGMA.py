721f7b9a82b3d265c7f54c11a761f09ad31f5ac9
Seirana Hashemi <seirana.hashemi@gmail.com>
2025-12-12 11:35:34 +0100

    Update stp3_generate_input_file_for_MAGMA.py

--- bin/stp3_generate_input_file_for_MAGMA.py
+++ bin/stp3_generate_input_file_for_MAGMA.py
@@ -6,11 +6,11 @@
 This program generates files needed for the MAGMA gene-based test.
 
 input:
-    ./scDRS/output/variants_with_rsID_PSC_WES_SAIGE.vcf 
+    ./scDRS/output/variants_with_rsID.vcf 
     .scDRS/data/SAIGE_single_marker_test.txt
 output:
-    ./scDRS/output/PSC_WES_SAIGE_for_step2.txt
-    ./scDRS/output/PSC_WES_SAIGE_for_MAGMA.txt
+    ./scDRS/output/files_for_step2.txt
+    ./scDRS/output/files_for_MAGMA.txt
 """
 
 from IPython import get_ipython
@@ -42,29 +42,18 @@ vcf = pd.read_csv(
 PSC_WES_for_MAGMA = vcf.loc[:, ['ID', 'CHROM', 'POS']].copy()
 PSC_WES_for_MAGMA.columns = ['Variant name', 'CHROM', 'GENPOS']
 
-file = './scDRS/output/PSC_WES_SAIGE_for_MAGMA'
+file = './scDRS/output/files_for_MAGMA'
 rw.write_txt(file, PSC_WES_for_MAGMA, ' ', False)
 
 # .............................................................................
-file = './scDRS/data/SAIGE_single_marker_test.txt'
-
-with open(file, 'rt') as f:
-    lines = f.readlines()
-
-clmns = lines[0]
-clmns = clmns.split()
-l = len(clmns)
-if clmns[l-1].endswith('\n'):
-    s = clmns[l-1]
-    sl = len(s)
-    clmns[l-1] = s[0:sl-1]
-
-y = pd.DataFrame([x.strip().split() for x in lines], columns=clmns)
-y.drop(y.index[0], axis=0, inplace=True)
-y.reset_index(drop=True, inplace=True)
-df = y
+file = './scDRS/data/sampleWES.zip'
+df = pd.read_csv(
+    file,
+    sep=r"\s+",
+    compression="zip"
+)
 
 PSC_WES_for_step2 = df.loc[:, ['MarkerID', 'p.value']].copy()
 PSC_WES_for_step2.loc[:, 'MarkerID'] = vcf.loc[:, 'ID']
 PSC_WES_for_step2.columns = ['SNP id', 'p-value']
-file = './scDRS/output/PSC_WES_SAIGE_for_step2'
+file = './scDRS/output/files_for_step2'
